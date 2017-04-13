7bb60aab31b5bda7cb6c6f78b73419de08f7340b
shreydan <shreyasdaniel2001@yahoo.co.in>
2017-04-13 14:43:47 +0530

    Fixed linear.py

--- code/Algorithms/Searching/linear.py
+++ code/Algorithms/Searching/linear.py
@@ -4,7 +4,7 @@ def linear(srchlist, srch):
 	"""check every element, kinda slow"""
 	
 	for element in srchlist:
-		if element = srch:
+		if element == srch:
 			return srchlist.index(element)
 		
 	return -1
