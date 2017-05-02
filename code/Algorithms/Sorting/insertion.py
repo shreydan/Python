975ef9f531207a5aac45c871b535f427a1b19942
shreydan <shreyasdaniel2001@yahoo.co.in>
2017-05-02 23:33:45 +0530

    Added insertion sort algorithm

--- code/Algorithms/Sorting/insertion.py
+++ code/Algorithms/Sorting/insertion.py
@@ -3,16 +3,13 @@
 def insertion(sortlist):
     """starts from index 1, sorts everything behind and moves forward."""
 
-    for index in range(1,len(sortlist)):
-        value = sortlist[index]
-        i = index-1
-        while i>=0:
-            if value < sortlist[i]:
-                sortlist[i+1] = sortlist[i]
-                sortlist[i] = value
-                i-=1
-            else:
-                break
-                
+    for i in range(1,len(sortlist)):
+        element = sortlist[i]
+        j = i
+        while j > 0 and sortlist[j-1] > element:
+            sortlist[j] = sortlist[j-1]
+            j = j - 1
+        sortlist[j] = element
+    
     return sortlist
                 
