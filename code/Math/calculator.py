2ee31644dad0a3dfe0e66a4db2e391cc79490f5a
shreydan <shreyasdaniel2001@yahoo.co.in>
2017-05-09 16:32:01 +0530

    More functions: calculator.py

--- code/Math/calculator.py
+++ code/Math/calculator.py
@@ -3,26 +3,36 @@ Written by: Shreyas Daniel - github.com/shreydan
 Description: Uses Pythons infamous eval() function as a way to implement calculator
 """
 
+import math
+
+
 def calc(k):
 
-    for i in k.lower():
-        if i in 'abcdefghijklmnopqrstuvwxyz!@#$%&\{}[]:;<>?':
-            print ("INVALID")
-            exit()
+    functions = ['sin','cos','tan','sqrt','pi']    
+    
+    
+    for i in functions:
+        if i in k.lower():
+            withmath = 'math.' + i
+            k = k.replace(i,withmath)
     
     try:
         k = eval(k)
     except ZeroDivisionError:
         print ("Can't divide by 0")
         exit()
+    except NameError:
+        print ("Invalid input")
+        exit()
     
     return k
 
 
-print ("\nAvoid alphabets, symbols, = and ? characters in the input.\n")
+print ("\nScientific Calculator\nEg: pi * sin(90) - sqrt(81)")
 
-k = input("What is ")
+k = input("\nWhat is ")
 
+k = k.replace(' ','')
 k = k.replace('^','**')
 k = k.replace('=','')
 k = k.replace('?','')
