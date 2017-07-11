90465d08fa650f9f568380af874bf716a1105e81
shreydan <shreyasdaniel2001@yahoo.co.in>
2017-07-11 19:51:09 +0530

    better calculator.py

--- code/Math/calculator.py
+++ code/Math/calculator.py
@@ -1,54 +1,84 @@
 """
-Written by: Shreyas Daniel - github.com/shreydan
-Description: Uses Pythons infamous eval() function as a way to implement calculator
-
+Written by  : Shreyas Daniel - github.com/shreydan
+Description : Uses Pythons eval() function
+              as a way to implement calculator.
+             
 Functions available:
-
-+ : addition
-- : subtraction
-* : multiplication
-/ : division
-% : percentage
-sin: sine(rad)
-cos: cosine(rad)
-tan: tangent(rad)
-sqrt: square_root(n)
-pi: 3.141......
+--------------------------------------------
+                         + : addition
+                         - : subtraction
+                         * : multiplication
+                         / : division
+                         % : percentage
+                         e : 2.718281...
+                        pi : 3.141592...
+                      sine : sin(rad)
+                    cosine : cos(rad)
+                   tangent : tan(rad)
+               square root : sqrt(n)
+  round to nearest integer : round(n)
+convert degrees to radians : rad(deg)
 """
 
 import math
+import sys
 
 
 def calc(k):
+    
+    k = k.replace(' ', '')
+    k = k.replace('^', '**')
+    k = k.replace('=', '')
+    k = k.replace('?', '')
+    k = k.replace('%', '/100')
+    k = k.replace('rad', 'radians')
 
-    functions = ['sin','cos','tan','sqrt','pi']    
-    
-    
+    functions = ['sin', 'cos', 'tan', 'sqrt', 'pi', 'radians', 'e'] 
+
     for i in functions:
         if i in k.lower():
             withmath = 'math.' + i
-            k = k.replace(i,withmath)
-    
+            k = k.replace(i, withmath)
+
     try:
         k = eval(k)
     except ZeroDivisionError:
-        print ("Can't divide by 0")
+        print("Can't divide by 0")
         exit()
     except NameError:
-        print ("Invalid input")
+        print('Invalid input')
         exit()
-    
+    except AttributeError:
+        print('Check usage method')
+        exit()
+        
     return k
 
 
-print ("\nScientific Calculator\nEg: pi * sin(90) - sqrt(81)")
+def result(k):
+    print("\n" + str(calc(k)))
 
-k = input("\nWhat is ")
 
-k = k.replace(' ','')
-k = k.replace('^','**')
-k = k.replace('=','')
-k = k.replace('?','')
-k = k.replace('%','/100')
+def main():
+    
+    print("\nScientific Calculator\nEg: sin(rad(90)) + 50% * (sqrt(16)) + round(1.42^2)\nEnter quit to exit")
 
-print ("\n" + str(calc(k)))
+    if sys.version_info.major >= 3:
+        while True:
+            k = input("\nWhat is ")
+            if k == 'quit':
+                break
+            result(k)
+
+    else:
+        while True:
+            k = raw_input("\nWhat is ")
+            if k == 'quit':
+                break
+            result(k)
+
+
+if __name__ == '__main__':
+    main()
+
+# improvements by me and contributors at github.com/geekcomputers/python
