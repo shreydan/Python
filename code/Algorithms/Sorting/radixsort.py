75df9662be086212c9be4ea44557cf25f14bf206
Saphal patro <saphal1998@gmail.com>
2018-10-04 15:58:45 +0530

    Added radix sort

---
+++ code/Algorithms/Sorting/radixsort.py
@@ -0,0 +1,13 @@
+# Method to do Radix Sort 
+def radixSort(arr): 
+  
+    # Find the maximum number to know number of digits 
+    max1 = max(arr) 
+  
+    # Do counting sort for every digit. Note that instead 
+    # of passing digit number, exp is passed. exp is 10^i 
+    # where i is current digit number 
+    exp = 1
+    while max1/exp > 0: 
+        countingSort(arr,exp) 
+        exp *= 10
