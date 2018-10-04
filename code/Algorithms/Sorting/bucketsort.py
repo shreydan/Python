12b887093095312d3e66d70ac488559f551a4826
Saphal patro <saphal1998@gmail.com>
2018-10-04 16:06:32 +0530

    Added bucket sort

---
+++ code/Algorithms/Sorting/bucketsort.py
@@ -0,0 +1,21 @@
+def bucket_sort(alist):
+    largest = max(alist)
+    length = len(alist)
+    size = largest/length
+ 
+    buckets = [[] for _ in range(length)]
+    for i in range(length):
+        j = int(alist[i]/size)
+        if j != length:
+            buckets[j].append(alist[i])
+        else:
+            buckets[length - 1].append(alist[i])
+ 
+    for i in range(length):
+        insertion_sort(buckets[i])
+ 
+    result = []
+    for i in range(length):
+        result = result + buckets[i]
+ 
+    return result
